2568962427873a7c7fd77e59b745f819c9c986be
Ido Schimmel <idosch@idosch.org>
2019-03-02 21:26:47 +0200

    compile: Refactor compile script
    Signed-off-by: Ido Schimmel <idosch@idosch.org>

--- scripts/kernel/compile.sh
+++ scripts/kernel/compile.sh
@@ -10,112 +10,229 @@ config_disable()
 	scripts/config -d $1
 }
 
-config_set_str()
-{
-	scripts/config --set-str $1 $2
-}
-
-config_set_val()
-{
-	scripts/config --set-val $1 $2
-}
-
 config_module()
 {
 	scripts/config -m $1
 }
 
-prepare_dir()
+config_set_str()
 {
-	make clean &> /dev/null
-	make defconfig &> /dev/null
+	local opt=$1; shift
+	local str=$1; shift
+
+	scripts/config --set-str $opt $str
 }
 
-kernel_config()
+config_set_val()
 {
-	# General
+	local opt=$1; shift
+	local val=$1; shift
+
+	scripts/config --set-val $opt $val
+}
+
+config()
+{
+	# Start with a relatively minimal config
+	make defconfig &> /dev/null
+
+	# General setup
 	config_set_str CONFIG_LOCALVERSION "-custom"
 	config_enable CONFIG_BPF_SYSCALL
+	config_enable CONFIG_CGROUPS
 	config_enable CONFIG_CGROUP_BPF
-	config_module CONFIG_NETLINK_DIAG
-	config_disable CONFIG_SECURITY_SELINUX
-	# Bus Options
-	config_disable CONFIG_PCCARD
+	config_enable CONFIG_IKCONFIG
+	config_enable CONFIG_IKCONFIG_PROC
+	config_enable CONFIG_HIGH_RES_TIMERS
+	config_enable CONFIG_MODULES
+
+	# Power management
+	config_disable CONFIG_SUSPEND
+	config_disable CONFIG_HIBERNATION
+	config_disable CONFIG_PM
+
+	# Block layer
+	config_disable CONFIG_PARTITION_ADVANCED
+
 	# Networking support
-	config_disable CONFIG_XFRM_USER
+	config_disable CONFIG_HAMRADIO
+	config_disable CONFIG_RFKILL
+	config_enable CONFIG_NET
+	config_enable CONFIG_PACKET
+	config_enable CONFIG_PACKET_DIAG
+	config_enable CONFIG_UNIX
+	config_enable CONFIG_INET
+	config_disable CONFIG_IP_PNP
+	config_enable CONFIG_IP_MULTICAST
+	config_enable CONFIG_IP_ADVANCED_ROUTER
+	config_enable CONFIG_IP_MULTIPLE_TABLES
+	config_enable CONFIG_IP_ROUTE_MULTIPATH
 	config_module CONFIG_NET_IPIP
 	config_module CONFIG_NET_IPGRE_DEMUX
 	config_module CONFIG_NET_IPGRE
-	config_module CONFIG_IPV6_GRE
+	config_enable CONFIG_IP_MROUTE
 	config_enable CONFIG_IP_MROUTE_MULTIPLE_TABLES
-	config_disable CONFIG_TCP_MD5SIG
-	config_disable CONFIG_INET6_AH
-	config_disable CONFIG_INET6_ESP
+	config_enable CONFIG_IP_PIMSM_V1
+	config_enable CONFIG_IP_PIMSM_V2
+	config_disable CONFIG_XFRM_USER
+	config_disable CONFIG_INET_XFRM_MODE_TRANSPORT
+	config_disable CONFIG_INET_XFRM_MODE_TUNNEL
+	config_disable CONFIG_INET_XFRM_MODE_BEET
+	config_enable CONFIG_INET_DIAG
+	config_module CONFIG_IPV6
 	config_disable CONFIG_INET6_XFRM_MODE_TRANSPORT
 	config_disable CONFIG_INET6_XFRM_MODE_TUNNEL
 	config_disable CONFIG_INET6_XFRM_MODE_BEET
 	config_disable CONFIG_IPV6_SIT
 	config_enable CONFIG_IPV6_MULTIPLE_TABLES
 	config_enable CONFIG_IPV6_MROUTE
+	config_enable CONFIG_IPV6_MROUTE_MULTIPLE_TABLES
+	config_enable CONFIG_IPV6_PIMSM_V2
+	config_module CONFIG_IPV6_GRE
+	config_disable CONFIG_INET6_AH
+	config_disable CONFIG_INET6_ESP
+	config_disable CONFIG_NETWORK_SECMARK
 	config_disable CONFIG_NETLABEL
 	config_disable CONFIG_NETFILTER
 	config_module CONFIG_BRIDGE
+	config_enable CONFIG_BRIDGE_IGMP_SNOOPING
 	config_module CONFIG_VLAN_8021Q
 	config_enable CONFIG_BRIDGE_VLAN_FILTERING
-	config_enable CONFIG_BRIDGE_IGMP_SNOOPING
+	config_enable CONFIG_NET_SCHED
 	config_module CONFIG_NET_SCH_PRIO
 	config_module CONFIG_NET_SCH_RED
-	config_module CONFIG_NET_SCH_INGRESS
+	config_enable CONFIG_NET_SCH_DEFAULT
+	config_disable CONFIG_NET_EMATCH
 	config_module CONFIG_NET_CLS_FLOWER
 	config_module CONFIG_NET_CLS_MATCHALL
+	config_enable CONFIG_NET_CLS_ACT
 	config_module CONFIG_NET_ACT_GACT
 	config_module CONFIG_NET_ACT_MIRRED
 	config_module CONFIG_NET_ACT_SAMPLE
 	config_module CONFIG_NET_ACT_VLAN
+	config_module CONFIG_NET_SCH_INGRESS
 	config_enable CONFIG_DCB
+	config_module CONFIG_OPENVSWITCH
+	config_disable CONFIG_OPENVSWITCH_GRE
+	config_disable CONFIG_OPENVSWITCH_VXLAN
+	config_module CONFIG_NETLINK_DIAG
 	config_enable CONFIG_NET_SWITCHDEV
 	config_enable CONFIG_NET_L3_MASTER_DEV
 	config_module CONFIG_NET_PKTGEN
-	config_disable CONFIG_HAMRADIO
-	config_disable CONFIG_CFG80211
-	config_disable CONFIG_RFKILL
+	config_disable CONFIG_WIRELESS
 	config_enable CONFIG_NET_DEVLINK
-	config_enable CONFIG_OPENVSWITCH
-	# File systems
-	config_module CONFIG_9P_FS
-	config_module CONFIG_XFS_FS
-	# Device Drivers
+	config_module CONFIG_NET_9P
+	config_enable CONFIG_NET_NS
+
+	# Device drivers
+	config_disable CONFIG_PCCARD
+	config_disable CONFIG_CONNECTOR
 	config_disable CONFIG_MACINTOSH_DRIVERS
+	config_disable CONFIG_FDDI
+	config_disable USB_NET_DRIVERS
+	config_enable CONFIG_PCI
+	config_enable CONFIG_PCI_MSI
+	config_enable CONFIG_DEVTMPFS
+	config_enable CONFIG_DEVTMPFS_MOUNT
+	config_module CONFIG_ATA
+	config_enable CONFIG_NETDEVICES
+	config_enable CONFIG_NET_CORE
 	config_module CONFIG_BONDING
 	config_module CONFIG_DUMMY
 	config_module CONFIG_NET_TEAM
 	config_module CONFIG_NET_TEAM_MODE_LOADBALANCE
-	config_module CONFIG_TUN
-	config_module CONFIG_VETH
-	config_module CONFIG_NET_VRF
 	config_module CONFIG_MACVLAN
 	config_module CONFIG_VXLAN
+	config_module CONFIG_TUN
+	config_module CONFIG_VETH
+	config_module CONFIG_NLMON
+	config_module CONFIG_NET_VRF
+	config_disable CONFIG_NETCONSOLE
+	config_enable CONFIG_ETHERNET
+	config_module CONFIG_E1000
+	config_module CONFIG_E1000E
 	config_module CONFIG_IGB
+	config_disable CONFIG_E100
+	config_disable CONFIG_NET_VENDOR_BROADCOM
+	config_disable CONFIG_NET_VENDOR_DEC
+	config_disable CONFIG_NET_VENDOR_MARVELL
+	config_disable CONFIG_NET_VENDOR_REALTEK
+	config_disable CONFIG_PHYLIB
+	config_disable CONFIG_MDIO_DEVICE
 	config_module CONFIG_MLXSW_CORE
-	config_enable CONFIG_MLXSW_CORE_HWMON
-	config_enable CONFIG_MLXSW_CORE_THERMAL
-	config_module CONFIG_MLXSW_PCI
-	config_module CONFIG_MLXSW_I2C
 	config_disable CONFIG_MLXSW_SWITCHIB
 	config_disable CONFIG_MLXSW_SWITCHX2
-	config_module CONFIG_MLXSW_SPECTRUM
-	config_enable CONFIG_MLXSW_SPECTRUM_DCB
-	config_enable CONFIG_MLXSW_MINIMAL
-	config_disable CONFIG_FORCEDETH
-	config_disable CONFIG_FDDI
-	config_disable USB_NET_DRIVERS
 	config_disable CONFIG_WLAN
+	config_module CONFIG_VIRTIO_PCI
+	config_module CONFIG_VIRTIO_BALLOON
+	config_module CONFIG_VIRTIO_INPUT
+	config_module CONFIG_I2C_MLXCPLD
+	config_module CONFIG_I2C_MUX
+	config_module CONFIG_I2C_MUX_MLXCPLD
+	config_enable CONFIG_NEW_LEDS
+	config_module CONFIG_LEDS_CLASS
+	config_module CONFIG_LEDS_MLXCPLD
+	config_module CONFIG_LEDS_MLXREG
+	config_enable CONFIG_MELLANOX_PLATFORM
+	config_enable CONFIG_REGMAP
+	config_module CONFIG_MLXREG_HOTPLUG
+	config_module CONFIG_MLXREG_IO
+	config_module CONFIG_MLX_PLATFORM
+	config_module CONFIG_SENSORS_MLXREG_FAN
+	config_disable CONFIG_UEVENT_HELPER
+	config_disable CONFIG_SOUND
 	config_disable CONFIG_AGP
 	config_disable CONFIG_DRM
 	config_disable CONFIG_BACKLIGHT_LCD_SUPPORT
 	config_disable CONFIG_LOGO
-	config_disable CONFIG_SOUND
-	config_module CONFIG_LEDS_MLXCPLD
+	config_disable CONFIG_PTP_1588_CLOCK
+
+	# File systems
+	config_module CONFIG_XFS_FS
+	config_module CONFIG_EXT4_FS
+	config_module CONFIG_9P_FS
+	config_module CONFIG_NFS_FS
+	config_module CONFIG_NFS_V4
+	config_enable CONFIG_NFS_V4_1
+	config_enable CONFIG_NFS_V4_2
+	config_module CONFIG_AUTOFS_FS
+	config_enable CONFIG_TMPFS
+	config_enable CONFIG_DEBUG_FS
+	config_disable CONFIG_ISO9660_FS
+	config_disable CONFIG_MSDOS_FS
+	config_disable CONFIG_VFAT_FS
+	config_disable CONFIG_MISC_FILESYSTEMS
+
+	# Security
+	config_disable SECURITY_SELINUX
+	config_disable CONFIG_SECURITY
+	config_disable CONFIG_PAGE_TABLE_ISOLATION
+
+	# Cryptographic API
+	config_disable CONFIG_CRYPTO_RSA
+	config_disable CONFIG_CRYPTO_MANAGER_DISABLE_TESTS
+	config_disable CONFIG_CRYPTO_AUTHENC
+	config_disable CONFIG_CRYPTO_CCM
+	config_disable CONFIG_CRYPTO_GCM
+	config_disable CONFIG_CRYPTO_ECHAINIV
+	config_disable CONFIG_CRYPTO_CBC
+	config_disable CONFIG_CRYPTO_CTR
+	config_disable CONFIG_CRYPTO_SEQIV
+	config_disable CONFIG_CRYPTO_NULL
+	config_disable CONFIG_CRYPTO_CMAC
+	config_disable CONFIG_CRYPTO_GHASH
+	config_disable CONFIG_CRYPTO_SHA1
+	config_disable CONFIG_CRYPTO_ARC4
+	config_disable CONFIG_CRYPTO_DES
+	config_disable CONFIG_CRYPTO_DRBG_MENU
+	config_disable CONFIG_CRYPTO_JITTERENTROPY
+	config_disable CONFIG_CRYPTO_HW
+	config_disable CONFIG_ASYMMETRIC_KEY_TYPE
+	config_disable CONFIG_CRYPTO_SHA256
+	config_disable CONFIG_CRYPTO_HMAC
+	config_disable CONFIG_CRYPTO_GF128MUL
+	config_disable CONFIG_CRYPTO_MANAGER
+
 	# Virtualization
 	config_module CONFIG_KVM
 	config_module CONFIG_KVM_INTEL
@@ -127,18 +244,19 @@ kernel_config()
 	config_module CONFIG_VIRTIO_NET
 	config_module CONFIG_VIRTIO_CONSOLE
 	config_module CONFIG_SCSI_VIRTIO
-	# Firmware
-	config_set_str CONFIG_EXTRA_FIRMWARE "mellanox/mlxsw_spectrum-13.1910.622.mfa2"
 }
 
-general_debug()
+debug_enable()
 {
-	# General debug options
+	# Debug options
+	## General debug options
 	config_enable CONFIG_PREEMPT
 	config_enable CONFIG_DEBUG_PREEMPT
 	config_enable CONFIG_DEBUG_INFO
 	config_enable CONFIG_UNWINDER_ORC
-	# Lock debugging
+	config_enable CONFIG_DYNAMIC_DEBUG
+	config_enable CONFIG_DEBUG_NOTIFIERS
+	## Lock debugging
 	config_enable CONFIG_LOCKDEP
 	config_enable CONFIG_PROVE_LOCKING
 	config_enable CONFIG_DEBUG_ATOMIC_SLEEP
@@ -146,15 +264,15 @@ general_debug()
 	config_enable CONFIG_DEBUG_MUTEXES
 	config_enable CONFIG_DEBUG_SPINLOCK
 	config_enable CONFIG_LOCK_STAT
-	# Memory debugging
+	## Memory debugging
 	config_enable CONFIG_DEBUG_VM
 	config_enable CONFIG_FORTIFY_SOURCE
 	config_enable CONFIG_KASAN
 	config_enable CONFIG_KASAN_EXTRA
 	config_enable CONFIG_KASAN_INLINE
-	# Reference counting debugging
+	## Reference counting debugging
 	config_enable CONFIG_REFCOUNT_FULL
-	# Lockups debugging
+	## Lockups debugging
 	config_enable CONFIG_LOCKUP_DETECTOR
 	config_enable CONFIG_SOFTLOCKUP_DETECTOR
 	config_enable CONFIG_HARDLOCKUP_DETECTOR
@@ -162,31 +280,27 @@ general_debug()
 	config_enable CONFIG_WQ_WATCHDOG
 	config_enable CONFIG_DETECT_HUNG_TASK
 	config_set_val CONFIG_DEFAULT_HUNG_TASK_TIMEOUT 120
-	# Undefined behavior debugging
+	## Undefined behavior debugging
 	config_enable CONFIG_UBSAN
 	config_enable CONFIG_UBSAN_SANITIZE_ALL
 	config_disable CONFIG_UBSAN_ALIGNMENT
 	config_disable CONFIG_UBSAN_NULL
-	# Tracing
+	## Tracing
 	config_enable CONFIG_FUNCTION_TRACER
 	config_enable CONFIG_FUNCTION_GRAPH_TRACER
 	config_enable CONFIG_STACK_TRACER
 	config_enable CONFIG_DYNAMIC_FTRACE
 	config_enable CONFIG_FTRACE_SYSCALLS
-	# Code coverage
+	## Code coverage
 	config_enable CONFIG_KCOV
-	# syzkaller
-	config_enable CONFIG_USER_NS
 }
 
-heavy_debug()
+more_debug_enable()
 {
-	# General debug options
-	config_enable CONFIG_CRASH_DUMP
-	config_enable CONFIG_PANIC_ON_OOPS
-	config_enable CONFIG_DYNAMIC_DEBUG
-	config_enable CONFIG_DEBUG_NOTIFIERS
-	# Memory debugging
+	debug_enable
+
+	# More debug options
+	## Memory debugging
 	config_enable CONFIG_SLUB_DEBUG
 	config_enable CONFIG_SLUB_DEBUG_ON
 	config_enable CONFIG_DEBUG_PAGEALLOC
@@ -203,7 +317,7 @@ heavy_debug()
 	config_enable CONFIG_DEBUG_OBJECTS_WORK
 	config_enable CONFIG_DEBUG_OBJECTS_PERCPU_COUNTER
 	config_enable CONFIG_DMA_API_DEBUG
-	# Lock debugging
+	## Lock debugging
 	config_enable CONFIG_DEBUG_LOCK_ALLOC
 	config_enable CONFIG_PROVE_LOCKING
 	config_enable CONFIG_LOCK_STAT
@@ -211,137 +325,47 @@ heavy_debug()
 	config_enable CONFIG_SPARSE_RCU_POINTER
 }
 
-compile()
+usage()
 {
-	make olddefconfig &> /dev/null
-
-	make -j`nproc`
+	cat <<EOF
+usage: ${0##*/} OPTS
+        -c          Only create config file. Do not compile
+	-m          Use modules when possible
+	-d          Enable debug options (slow)
+	-D          Enable even more debug options (very slow)
+EOF
 }
 
-die()
-{
-	local _ret=$2
-	test -n "$_ret" || _ret=1
-	test "$_PRINT_HELP" = yes && print_help >&2
-	echo "$1" >&2
-	exit ${_ret}
-}
+while getopts ":cmdDh" opt; do
+	case ${opt} in
+		c) CONFIG_ONLY=yes;;
+		m) MODULES=yes;;
+		d) DEBUG=yes;;
+		D) MORE_DEBUG=yes;;
+		h) usage; exit 0;;
+		\?) usage; exit 1;;
+	esac
+done
 
-begins_with_short_option()
-{
-	local first_option all_short_options
-	all_short_options='dDvah'
-	first_option="${1:0:1}"
-	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
-}
+config
 
-# THE DEFAULTS INITIALIZATION - OPTIONALS
-_arg_debug="off"
-_arg_heavy_debug="off"
-_arg_vm="off"
-_arg_auto_version="off"
-
-print_help ()
-{
-	printf '%s\n' "Script to ease kernel configuration and compilation"
-	printf 'Usage: %s [-d|--(no-)debug] [-D|--(no-)heavy-debug] [-v|--(no-)vm] [-a|--(no-)auto-version] [-h|--help]\n' "$0"
-	printf '\t%s\n' "-d,--debug,--no-debug: Enable useful debug options (off by default)"
-	printf '\t%s\n' "-D,--heavy-debug,--no-heavy-debug: Enable a lot of debug options. Results in a very slow kernel. (off by default)"
-	printf '\t%s\n' "-v,--vm,--no-vm: Configure kernel for a VM (no modules) (off by default)"
-	printf '\t%s\n' "-a,--auto-version,--no-auto-version: Configure the kernel with LOCALVERSION_AUTO (off by default)"
-	printf '\t%s\n' "-h,--help: Prints help"
-}
-
-parse_commandline ()
-{
-	while test $# -gt 0
-	do
-		_key="$1"
-		case "$_key" in
-			-d|--no-debug|--debug)
-				_arg_debug="on"
-				test "${1:0:5}" = "--no-" && _arg_debug="off"
-				;;
-			-d*)
-				_arg_debug="on"
-				_next="${_key##-d}"
-				if test -n "$_next" -a "$_next" != "$_key"
-				then
-					begins_with_short_option "$_next" && shift && set -- "-d" "-${_next}" "$@" || die "The short option '$_key' can't be decomposed to ${_key:0:2} and -${_key:2}, because ${_key:0:2} doesn't accept value and '-${_key:2:1}' doesn't correspond to a short option."
-				fi
-				;;
-			-D|--no-heavy-debug|--heavy-debug)
-				_arg_heavy_debug="on"
-				test "${1:0:5}" = "--no-" && _arg_heavy_debug="off"
-				;;
-			-D*)
-				_arg_heavy_debug="on"
-				_next="${_key##-D}"
-				if test -n "$_next" -a "$_next" != "$_key"
-				then
-					begins_with_short_option "$_next" && shift && set -- "-D" "-${_next}" "$@" || die "The short option '$_key' can't be decomposed to ${_key:0:2} and -${_key:2}, because ${_key:0:2} doesn't accept value and '-${_key:2:1}' doesn't correspond to a short option."
-				fi
-				;;
-			-v|--no-vm|--vm)
-				_arg_vm="on"
-				test "${1:0:5}" = "--no-" && _arg_vm="off"
-				;;
-			-v*)
-				_arg_vm="on"
-				_next="${_key##-v}"
-				if test -n "$_next" -a "$_next" != "$_key"
-				then
-					begins_with_short_option "$_next" && shift && set -- "-v" "-${_next}" "$@" || die "The short option '$_key' can't be decomposed to ${_key:0:2} and -${_key:2}, because ${_key:0:2} doesn't accept value and '-${_key:2:1}' doesn't correspond to a short option."
-				fi
-				;;
-			-a|--no-auto-version|--auto-version)
-				_arg_auto_version="on"
-				test "${1:0:5}" = "--no-" && _arg_auto_version="off"
-				;;
-			-a*)
-				_arg_auto_version="on"
-				_next="${_key##-a}"
-				if test -n "$_next" -a "$_next" != "$_key"
-				then
-					begins_with_short_option "$_next" && shift && set -- "-a" "-${_next}" "$@" || die "The short option '$_key' can't be decomposed to ${_key:0:2} and -${_key:2}, because ${_key:0:2} doesn't accept value and '-${_key:2:1}' doesn't correspond to a short option."
-				fi
-				;;
-			-h|--help)
-				print_help
-				exit 0
-				;;
-			-h*)
-				print_help
-				exit 0
-				;;
-			*)
-				_PRINT_HELP=yes die "FATAL ERROR: Got an unexpected argument '$1'" 1
-				;;
-		esac
-		shift
-	done
-}
-
-parse_commandline "$@"
-
-prepare_dir
-kernel_config
-
-if [ "$_arg_auto_version" = "on" ]; then
-	config_enable CONFIG_LOCALVERSION_AUTO
+if [[ "$DEBUG" = "yes" ]]; then
+	debug_enable
 fi
 
-if [ "$_arg_debug" = "on" ]; then
-	general_debug
+if [[ "$MORE_DEBUG" = "yes" ]]; then
+	more_debug_enable
 fi
 
-if [ "$_arg_heavy_debug" = "on" ]; then
-	general_debug
-	heavy_debug
-fi
-
-if [ "$_arg_vm" = "on" ]; then
+if [[ "$MODULES" = "" ]]; then
 	config_disable CONFIG_MODULES
 fi
 
-compile
+# Set new symbols to their default
+make olddefconfig &> /dev/null
+
+if [[ "$CONFIG_ONLY" = "yes" ]]; then
+	exit 0
+fi
+
+make -j`nproc`
